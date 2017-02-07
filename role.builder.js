d144a17f188dbbaf84269d7745cf90c951130946
JellyCo9064 <theredmoo@gmail.com>
2017-02-07 14:28:29 -0800

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,10 +1,41 @@
-module.exports = {
+var roleBuilder = {
 	name: 'builder',
 	priority:2,
 	max:2,
 	min:1,
 	parts: ['WORK', 'MOVE', 'CARRY'],
 	loop: function(creep){
+		
+		if (creep.memory.building && creep.carry.energy == 0)
+		{
+			creep.memory.building = false;
+			creep.say('harvesting');
+		}
+		if (!creep.memory.building && creep.carry.energy == creep.carryCapacity)
+		{
+			creep.memory.building = true;
+			creep.say('building');
+		}
+		if (creep.memory.building)
+		{
+			var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+			if (targets.length > 0)
+			{
+				if(creep.build(targets[0]) == ERR_NOT_IN_RANGE)
+				{
+					creep.moveTo(targets[0]);   
+				}
+			}
+		}
+		else
+		{
+			var sources = creep.room.find(FIND_SOURCES);
+			if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+			{
+				creep.moveTo(sources[0]);	
+			}
+		}
 
 	}
 };
+module.exports = roleBuilder;
